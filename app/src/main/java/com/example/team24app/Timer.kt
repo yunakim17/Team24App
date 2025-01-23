1733cff1f0201c36c84e57562887b2c0ec7bf769
ekfud <tlsekfud1393@naver.com>
2025-01-23 18:43:06 +0900

    타이머 kt 추가 - 시작 버튼의 string 추가 - 현재 날짜 수정/시분초 갱신/시작 및 정지

--- app/src/main/java/com/example/team24app/Timer.kt
+++ app/src/main/java/com/example/team24app/Timer.kt
@@ -1,20 +1,71 @@
 package com.example.team24app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class Timer : AppCompatActivity() {
+    lateinit var tvdate : TextView
+    lateinit var tvhour : TextView
+    lateinit var tvmin : TextView
+    lateinit var tvsec : TextView
+    lateinit var ssbtn : Button
+
+    var time = 0
+    private var timerTask: Timer? = null
+    private var isRunning=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_timer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvdate = findViewById<TextView>(R.id.tvDate)
+        tvhour = findViewById<TextView>(R.id.tvHours)
+        tvmin = findViewById<TextView>(R.id.tvMinutes)
+        tvsec = findViewById<TextView>(R.id.tvSeconds)
+        ssbtn = findViewById<Button>(R.id.startStopButton)
+
+        var now = Date()
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd")
+
+        tvdate.text = dateFormat.format(now)
+
+        ssbtn.setOnClickListener {
+            isRunning=!isRunning
+
+            if(isRunning){
+                start()
+            }else{
+                pause()
+            }
         }
+
+    }
+
+    private fun start(){
+        ssbtn.text = getString(R.string.stop)
+        timerTask = timer(period=1000){
+            time++
+
+            val hour = (time/60)/60
+            val min = (time/60)%60
+            val sec = time%60
+
+            runOnUiThread {
+                tvhour.text="$hour"
+                tvmin.text="$min"
+                tvsec.text="$sec"
+            }
+        }
+    }
+
+    private fun pause(){
+        ssbtn.text=getString(R.string.start)
+        timerTask?.cancel()
     }
 }
