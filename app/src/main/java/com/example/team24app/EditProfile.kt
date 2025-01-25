f80a4b007383154853f24385a4f6144895c7a734
ekfud <tlsekfud1393@naver.com>
2025-01-25 17:44:30 +0900

    UploadPost, EditProfile 추가 - 업로드 포스트에서 갤러리 오픈 함수들 추가 -프로필 편집의 버튼(저장) 활성화
    -1차 코딩 완료 테이블 수정 후 전반적으로 업데이트 예정

--- app/src/main/java/com/example/team24app/EditProfile.kt
+++ app/src/main/java/com/example/team24app/EditProfile.kt
@@ -1,20 +1,24 @@
 package com.example.team24app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class EditProfile : AppCompatActivity() {
+    lateinit var tvName : TextView
+    lateinit var tvDescrip : TextView
+    lateinit var btnSave : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        tvName = findViewById<TextView>(R.id.tvUserName)
+        tvDescrip = findViewById<TextView>(R.id.edtDescription)
+        btnSave = findViewById<Button>(R.id.btnSaveProfile)
+
+        btnSave.setOnClickListener {
+            //테이블에 수정 소개 문구와 이미지 경로 수정 예정
         }
     }
 }
--- app/src/main/java/com/example/team24app/UploadPost.kt
+++ app/src/main/java/com/example/team24app/UploadPost.kt
@@ -1,20 +1,83 @@
 package com.example.team24app
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class UploadPost : AppCompatActivity() {
+    lateinit var ivPhoto : ImageView
+    lateinit var btnPhoto : Button
+    lateinit var tvHour : TextView
+    lateinit var tvMin : TextView
+    lateinit var tvSec : TextView
+    lateinit var edtComment : EditText
+    lateinit var btnUpload : Button
+    private var imageUri : Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_upload_post)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        ivPhoto = findViewById<ImageView>(R.id.SelectedPhoto)
+        btnPhoto = findViewById<Button>(R.id.btnPhoto)
+        tvHour = findViewById<TextView>(R.id.tvHours)
+        tvMin = findViewById<TextView>(R.id.tvMinutes)
+        tvSec = findViewById<TextView>(R.id.tvSeconds)
+        edtComment = findViewById<EditText>(R.id.edtComment)
+        btnUpload = findViewById<Button>(R.id.btnUpload)
+
+        //시간 업로드는 타이머 버튼시 intent로 전달 예정
+
+        val readImagePermission = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) android.Manifest.permission.READ_MEDIA_IMAGES
+        else android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+        btnPhoto.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, readImagePermission)==PackageManager.PERMISSION_GRANTED){
+                //권한 확인 성공시 갤러리 오픈
+                openGallery()
+            }else{
+                requestPermissionLauncher.launch(readImagePermission)
+                //권한 실패시 재시도
+            }
         }
+
+        btnUpload.setOnClickListener {
+            //피드 테이블에 업로드
+        }
+    }
+
+    private val requestPermissionLauncher : ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        isGranted -> if (isGranted){
+            //권한 성공시 갤러리 오픈
+            openGallery()
+        }
+    }
+
+    private fun openGallery(){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        pickImageLauncher.launch(gallery)
+        //갤러리를 열기 위해 intent 생성, 갤러리 액티비티 실행
+    }
+
+    private val pickImageLauncher : ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result -> if(result.resultCode == RESULT_OK){
+                val data:Intent? = result.data
+                data?.data?.let{
+                    imageUri = it
+                    ivPhoto.setImageURI(imageUri)
+                }
+            }
     }
 }
