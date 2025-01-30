dd51a4d458b962a55cbea5b186284f3f56984762
HANNA <hanna2488@swu.ac.kr>
2025-01-30 23:27:58 +0900

    Main, Login commit

--- app/src/main/java/com/example/team24app/Login.kt
+++ app/src/main/java/com/example/team24app/Login.kt
@@ -1,20 +1,54 @@
 package com.example.team24app
 
+import android.adservices.adid.AdId
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+    lateinit var edtLoginId: EditText
+    lateinit var edtLoginPw: EditText
+    lateinit var btnLogin: Button
+    var DB:DBManager?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        DB = DBManager(this, "Login", null, 1)
+
+        edtLoginId = findViewById(R.id.edtLoginId)
+        edtLoginPw = findViewById(R.id.edtLoginPw)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin!!.setOnClickListener {
+            val user = edtLoginId!!.text.toString()
+            val pass = edtLoginPw!!.text.toString()
+
+            // 빈칸 제출시 Toast
+            if (user == "" || pass == "") {
+                Toast.makeText(this, "아이디와 비밀번호를 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val checkUserpass = DB!!.checkUserpass(user, pass)
+                // id 와 password 일치시
+                if (checkUserpass == true) {
+                    Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, Home::class.java) //로그인 되면 Home으로 넘어감
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this, "아이디와 비밀번호를 확인해 주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/team24app/MainActivity.kt
+++ app/src/main/java/com/example/team24app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.team24app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 버튼 ID 연결
+        val btnLogin = findViewById<Button>(R.id.btnIdLogin)
+        val btnSignup = findViewById<Button>(R.id.btnSignup)
+
+        // 로그인 버튼 클릭 시 Login.kt 이동
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
+
+        // 회원가입 버튼 클릭 시 Signup.kt 이동
+        btnSignup.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
     }
 }
